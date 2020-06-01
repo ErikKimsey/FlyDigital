5d2e26a63f6fb9fefd65a00d784797fa1291e680
Erik Kimsey <kimseyerik@gmail.com>
2020-06-01 10:14:13 -0400

    Created ParticleChargeManager script and added notes to it

---
+++ Assets/Scripts/ParticleChargeManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleChargeManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // -- On ReadyCast --
+    // * Play particles,
+    // **** 
+
+    // -- On Cast --
+    // * Stop Particles
+    // ****
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
