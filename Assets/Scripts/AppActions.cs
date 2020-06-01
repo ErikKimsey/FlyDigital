a48bb3987e6be07a79195bbe555226ce68e9b43c
Erik Kimsey <kimseyerik@gmail.com>
2020-06-01 09:34:26 -0400

    Cleaned up unnecessary methods in AppActions

--- Assets/Scripts/AppActions.cs
+++ Assets/Scripts/AppActions.cs
@@ -4,20 +4,8 @@ using UnityEngine;
 
 public class AppActions : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     public void QuitApp(){
       Debug.Log("QUIT APP");
       Application.Quit();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-      
     }
 }
