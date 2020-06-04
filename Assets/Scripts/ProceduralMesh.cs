f2b29f3a3ce3ceb180087d24c2b7fbc843d6a855
Erik Kimsey <kimseyerik@gmail.com>
2020-06-04 10:46:09 -0400

    Procedurally generating a triangle. Noice

--- Assets/Scripts/ProceduralMesh.cs
+++ Assets/Scripts/ProceduralMesh.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
+// [RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
 public class ProceduralMesh : MonoBehaviour
 {
     private Vector3[] vertices;
@@ -14,15 +14,31 @@ public class ProceduralMesh : MonoBehaviour
     private MeshRenderer pgMeshRenderer;
 
     void Start(){
-      pgMesh = new Mesh();
-      pgMeshFilter = gameObject.AddComponent<MeshFilter>();
-      pgMeshRenderer = gameObject.AddComponent<MeshRenderer>();
-      pgMeshFilter.mesh = pgMesh;
+      // pgMesh = new Mesh();
+      pgMesh = GetComponent<MeshFilter>().mesh;
+      // pgMeshRenderer = gameObject.AddComponent<MeshRenderer>();
+      // pgMesh = pgMeshFilter.mesh;
+      BuildMesh();
+      DrawMesh();
     }
 
     private void BuildMesh(){
+      vertices = new Vector3[]{
+        new Vector3(0f,0f,0f), 
+        new Vector3(0f, 0f, 1f),
+        new Vector3(1f, 0f, 1f)
+        // new Vector3(0f, 0f, -1f),
+        // new Vector3(0f, 0f, -1f),
+        // new Vector3(0f, 0f, -1f),
+      };
 
+      tris = new int[]{
+        0,1,2
+      };
     }
 
-
+    private void DrawMesh(){
+      pgMesh.vertices = vertices;
+      pgMesh.triangles = tris;
+    }
 }
