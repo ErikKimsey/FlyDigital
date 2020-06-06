dc59fdc6e7e610d51e55da8be5ad80375034035b
Erik Kimsey <kimseyerik@gmail.com>
2020-06-06 12:18:30 -0400

    Working out icosophere mesh logic

--- Assets/Scripts/IcosphereBuilder.cs
+++ Assets/Scripts/IcosphereBuilder.cs
@@ -4,20 +4,68 @@ using UnityEngine;
 
 public class IcosphereBuilder : MonoBehaviour
 {
-    private int maxVertices = 24;
+    [SerializeField]
+    private int maxVertices = 12;
+    [SerializeField]
+    private int maxTris = 20;
+    [SerializeField]
+    private int sideLength;
+
+    private float theta;
+
     private int[] tris;
     private Vector3[] vertices;
 
-    private int xPos, yPos, zPos;
+    private float xPos, yPos, zPos;
+
+    private int triIter = 0;
+    private int vertIter = 0;
 
     void Start()
     {
-      tris = new int[]{maxVertices};
-      vertices = new Vector3[]{};   
-      xPos = yPos = zPos = 0;
+      tris = new int[]{};
+      xPos = 0f;
+      yPos = 0f;
+      zPos = 0f;
+      theta = (1 + Mathf.Sqrt(5))/sideLength;
     }
 
+
+
     void CreateVertices(){
+
+      // (0, +/-1, theta)
+      // (+/-1 +/-theta, 0)
+      // (+/-theta, 0, +/-1)
+      // theta = (1 + sqrt(5))/2  ; where "2" is side length
+
+
+      vertices = new Vector3[]{};
+      for (int i = 0; i < maxVertices; i++){
+        for (int j = 0; j<3 ; j++)   {
+          vertices[vertIter] = new Vector3(i, i+1, theta + i);
+          vertices[vertIter + 1] = new Vector3(i + 1, theta + 1, i);
+          vertices[vertIter + 2] = new Vector3(theta + 1, i, i+1);
+        }
+        vertIter += 4;
+      }
+
+      void CreateTris(){
+
+        // Creating array of indices,
+        // indices pertain to that of the vertices[]
+        // indices exist in a pattern of: 
+        // (a, b, c), (b,c,d), (c,d,e), (d,e,f), (e,f,g),...
+
+        for (int i = 0; i < maxTris; i++){
+          for (int j = 0; j < 6; j++){
+            tris[i] = i;
+            tris[triIter + 1] = tris[triIter + 3] = i + 1;
+            // tris[triIter + 2] = 
+          }
+        }
+      }
+
 
       /**
       * iterate number of vertices,
@@ -36,16 +84,5 @@ public class IcosphereBuilder : MonoBehaviour
       * tris[10] = vertices[3],
       * tris[11] = vertices[5],
       */
-      for(int i=0; i<12; i++){
-        int triIndex = i;
-        for(int j=0; j<4; j++){
-          vertices[i] = new Vector3(xPos, yPos, zPos);
-          vertices[i+1] = vertices[i ] = new Vector3(xPos + j * 0.5f, yPos + 1, zPos + j * 0.5f);
-          vertices[i+2] = new Vector3(xPos + j + 1, yPos * j + 1, zPos * j + 0.5f);
-          tris[triIndex] ;
-          // tris = 1,2,3,/3,2,4,/3,4,5,/5,4,6,/5,6,7/
-        }
-
-      }
     }
 }
