2fdcbb7835e99d584f57ca15b549991ba722ceb8
Erik Kimsey <kimseyerik@gmail.com>
2020-06-12 08:22:41 -0400

    Converting to First Person

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,24 +1,7 @@
-#if ENABLE_INPUT_SYSTEM && ENABLE_INPUT_SYSTEM_PACKAGE
-#define USE_INPUT_SYSTEM
-    using UnityEngine.InputSystem;
-    using UnityEngine.InputSystem.Controls;
-#endif
-
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
-namespace Fishing.Assets.Scripts
+public class PlayerController : MonoBehaviour
 {
-    
-    public class PlayerController : MonoBehaviour {
-
-      private void Start()
-      {
-        
-      } 
-
-      private void FixedUpdate()
-      {
-          
-      }
-    }
 }
