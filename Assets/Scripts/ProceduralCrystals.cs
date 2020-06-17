f9ee56c699f8acf5c62ee509e2471fdc3a0f2254
Erik Kimsey <kimseyerik@gmail.com>
2020-06-17 11:05:15 -0400

    Recreated planet w added lakes, optimized asset

---
+++ Assets/Scripts/ProceduralCrystals.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProceduralCrystals
+{
+
+  private Vector3[] vertices;
+  public int maxVertices;
+
+  private void Start() {
+    
+  }   
+
+  public void CreateVertices(){
+    
+  }
+}
