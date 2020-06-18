0f10c9ec4f6cfd9f4dcc553e671d4b35f81c9762
Erik Kimsey <kimseyerik@gmail.com>
2020-06-18 12:51:24 -0400

    Used control logic from InHereIsOutThere. Created FPSCAmControl.cs to which an additional, right joystick is added

---
+++ Assets/Scripts/FPSCameraRotationFollow.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPSCameraRotationFollow : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float moveSpeed;
+    public FixedJoystick joystick;
+    public Transform player;
+    private float X, Y;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+      X = joystick.Horizontal * moveSpeed;
+      Y = joystick.Vertical * moveSpeed;
+      player.Rotate(0f, X, 0f);
+    }
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,15 +7,53 @@ public class PlayerMovement : MonoBehaviour
     // Start is called before the first frame update
     public float moveSpeed;
     private Vector3 moveDirection;
+    public FixedJoystick joystick;
+    public Rigidbody rb;
+    private float inputX, inputY;
+    private Vector3 currentDirectionVertex;
+
+    void HandleXMovement() {
+        inputX = joystick.Horizontal * moveSpeed * Time.deltaTime;
+        if (inputX > 0)
+        {
+            transform.Translate(Vector3.right * moveSpeed * Time.deltaTime);
+        }
+        if (inputX < 0)
+        {
+            transform.Translate(Vector3.left * moveSpeed * Time.deltaTime);
+        } 
+    }
+
+    void HandleZMovement() {
+      Debug.Log("joystick.Vertical");
+      Debug.Log(joystick.Vertical);
+        inputY = joystick.Vertical * moveSpeed * Time.deltaTime;
+        inputX = joystick.Horizontal * moveSpeed * Time.deltaTime;
+        if (inputY > 0)
+        {
+            currentDirectionVertex = Vector3.forward * Time.deltaTime;
+            transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+            HandleXMovement();
+        }
+        if (inputY < 0)
+        {
+            currentDirectionVertex = -Vector3.forward * Time.deltaTime;
+            transform.Translate(-Vector3.forward * moveSpeed * Time.deltaTime);
+            HandleXMovement();
+        }
+        if(inputY == 0){
+          transform.Translate(currentDirectionVertex);
+        }
+    }
 
     // Update is called once per frame
     void Update()
     {
-        moveDirection = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical")).normalized;
+        moveDirection = new Vector3(inputX, 0f, Input.GetAxisRaw("Vertical")).normalized;
     }
 
-    void FixedUpdate()
-    {
-        GetComponent<Rigidbody>().MovePosition(GetComponent<Rigidbody>().position + transform.TransformDirection(moveDirection));
+    void FixedUpdate() {
+      HandleZMovement();
+      GetComponent<Rigidbody>().MovePosition(GetComponent<Rigidbody>().position + transform.TransformDirection(moveDirection));
     }
 }
